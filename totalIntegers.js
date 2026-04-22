8c5300322fce9971639479c01c679a07b321b7b9
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-04-22 08:04:21 -0700

    feat: finish totalIntegers() successfully

--- totalIntegers.js
+++ totalIntegers.js
@@ -1,14 +1,25 @@
 const isObject = (val) => typeof val === "object" && val !== null;
 
 const totalIntegers = (data) => {
+  let count = 0;
+
   if (!isObject(data)) {
     return;
   }
 
   const objValues = Object.values(data);
-  console.log(objValues);
+
+  for (const val of objValues) {
+    if (Number.isInteger(val)) {
+      ++count;
+    } else if (isObject(val)) {
+      count += totalIntegers(val);
+    }
+  }
+
+  return count;
 };
 
 console.log(totalIntegers([[5], 3, [10, 20], 10])); // returns 5
-// totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]); // returns 7
-// totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } }); // returns 4
+console.log(totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]])); // returns 7
+console.log(totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } })); // returns 4
