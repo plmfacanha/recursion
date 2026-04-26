1bee915b0e191e88cdcef116815026fce953bc00
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-04-26 06:16:59 -0700

    feat: finish insertionSort()

--- insertionSort.js
+++ insertionSort.js
@@ -1,1 +1,20 @@
-const insertionSort = (arr) => {};
+const insertionSort = (arr) => {
+  if (Array.isArray(arr)) {
+    for (let i = 1; i < arr.length; ++i) {
+      let key = arr[i];
+      // Insert arr[i] into the sorted subarray arr[0:i-1]
+      j = i - 1;
+
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }
+
+      arr[j + 1] = key;
+    }
+  }
+
+  return arr;
+};
+
+console.log(insertionSort([5, 2, 4, 6, 1, 3])); // [1,2,3,4,5,6]
